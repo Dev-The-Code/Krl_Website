8b802c45f24e25e6000369bd24f35a2fe40541f2
devdaniyal <daniyalrazanoorani@gmail.com>
2020-03-30 12:24:17 +0500

    about carosel functionally

--- src/Components/About/OurCoreValues/ourCoreValues.js
+++ src/Components/About/OurCoreValues/ourCoreValues.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './ourCoreValues.css';
 import ACF from '../../../Assets/Images/acf.png';
+import { Carousel, Radio } from 'antd';
 
 let timer;
 
@@ -109,14 +110,35 @@ class OurCoreValues extends Component {
     const { Empathy, Transparency, listenCarefull, beHonest, beCreative, beArtistic } = this.state;
     return (
       <div>
-        <div className="d-none d-sm-block">
+        <div className="animatedup animatedFadeInUp fadeInUp">
+          <div className="row" style={{ marginTop: '7vw' }}>
+            <div className="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1"></div>
+            <div className="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 mobPading">
+              <p className="Ocv2">Core values</p>
+              <Carousel effect="fade" autoplay>
+                <div>
+                  <div className="Empathy"></div>
+                </div>
+                <div>
+                  <div className="Transparency"></div>
+                </div>
+                <div>
+                  <div className="listenCarefull"></div>
+                </div>
+                <div>
+                  <div className="beHonest"></div>
+                </div>
+              </Carousel>
+            </div>
+            <div className="col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1"></div>
+          </div>
+        </div>
+        {/* <div className="d-none d-sm-block">
           <div className="animatedup animatedFadeInUp fadeInUp">
             <div className="row" style={{ marginTop: '7vw' }}>
               <div className="col-xl-1 col-lg-1 col-md-1 col-sm-1"></div>
               <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 Ocv1">
-                <div className={this.state.carousel}>
-
-                </div>
+                <div className={this.state.carousel}></div>
               </div>
               <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 Ocv1">
                 <p className="Ocv2">Core values</p>
@@ -124,14 +146,14 @@ class OurCoreValues extends Component {
                 <p className={Transparency} onClick={this.coreSlider.bind(this, 'Transparency')}>Transparency</p>
                 <p className={beHonest} onClick={this.coreSlider.bind(this, 'beHonest')}>honest</p>
                 <p className={beCreative} onClick={this.coreSlider.bind(this, 'beCreative')}>Creativity</p>
-                {/* <p className={listenCarefull} onClick={this.coreSlider.bind(this, 'listenCarefull')}>listen carefully</p> */}
-                {/* <p className={beArtistic} onClick={this.coreSlider.bind(this, 'beArtistic')}>be artistic</p> */}
+                <p className={listenCarefull} onClick={this.coreSlider.bind(this, 'listenCarefull')}>listen carefully</p>
+                <p className={beArtistic} onClick={this.coreSlider.bind(this, 'beArtistic')}>be artistic</p>
               </div>
               <div className="col-xl-1 col-lg-1 col-md-1 col-sm-1"></div>
             </div>
           </div>
-        </div>
-        <div className="d-block d-sm-none">
+        </div> */}
+        {/* <div className="d-block d-sm-none">
           <div className="animatedup animatedFadeInUp fadeInUp">
             <div className="row">
               <div className="col-12">
@@ -150,19 +172,19 @@ class OurCoreValues extends Component {
                 <div className="col-6" style={{ textAlign: 'center' }}>
                   <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'Empathy')}>Empathy</p>
                   <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'beCreative')}>Creativity</p>
-                  {/* <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'listenCarefull')}>listen carefully</p> */}
+                  <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'listenCarefull')}>listen carefully</p>
                 </div>
                 <div className="col-6" style={{ textAlign: 'center' }}>
                   <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'Transparency')}>Transparency</p>
                   <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'beHonest')}>honest</p>
-                  {/* <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'beArtistic')}>be artistic</p> */}
+                  <p className="onColor_mob" onClick={this.coreSlider.bind(this, 'beArtistic')}>be artistic</p>
                 </div>
 
               </div>
             </div>
 
           </div>
-        </div>
+        </div> */}
         <br /><br /><br /><br />
       </div>
 
