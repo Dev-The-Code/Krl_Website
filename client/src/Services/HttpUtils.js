e9a225726a358452b6efb96c39e5b87f3d69387e
devawais <getawaisdeveloper@gmail.com>
2020-04-20 11:50:22 +0500

    change base url

--- client/src/Services/HttpUtils.js
+++ client/src/Services/HttpUtils.js
@@ -1,10 +1,10 @@
 import { AsyncStorage } from '@callstack/async-storage'
 
-const BASE_URL = 'http://localhost:8000/api';
+// const BASE_URL = 'http://localhost:8000/api';
 // const BASE_URL = 'https://pakjazba.com/api';
 // const BASE_URL = 'http://localhost:8000/api';
 //  const BASE_URL = 'https://pakjazba.com/api';
-//const BASE_URL = 'https://pure-hollows-17968.herokuapp.com/api';
+const BASE_URL = 'https://krl-website.herokuapp.com//api';
 //  const BASE_URL = 'https://getfit-server.herokuapp.com';
 
 
